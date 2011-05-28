c1ea5cd0203a29254a72283c4a90299a7bab56a7
bry <bry@bryanbibat.net>
2011-05-28 10:15:02 +0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+gem 'rake', '0.8.7'
+gem 'devise'
+gem 'paperclip'
+gem 'haml'
+gem 'mysql2', '0.2.7'
+
+group :development, :test do
+  gem 'capybara'
+  gem 'haml-rails'
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grind::Application.config.secret_token = '36911060aa29e780f5e5488b19a3181556f96dc8e3fef0068c97d5a7443728c8687a97a5a61ca1537b7c02e097454b8baa77038b7ebc2b6f4d3c68021244107e'
