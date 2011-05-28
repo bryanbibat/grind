ec1daa6298ec11cc23023ec5e8cd2ffc7a515e9e
bry <bry@bryanbibat.net>
2011-05-28 10:20:44 +0800

    add user fields

--- db/migrate/20110528021303_devise_create_users.rb
+++ db/migrate/20110528021303_devise_create_users.rb
@@ -5,17 +5,30 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.recoverable
       t.rememberable
       t.trackable
+      t.string :username, :null => false
+      t.string :display_name
+      t.integer :xp, :limit => 8, :default => 0
+      t.integer :health, :limit => 8, :default => 0
+      t.integer :energy, :limit => 8, :default => 0
+      t.integer :credits, :limit => 8, :default => 0
+      t.integer :melee, :limit => 8, :default => 5
+      t.integer :ranged, :limit => 8, :default => 5
+      t.integer :defense, :limit => 8, :default => 5
+      t.integer :agility, :limit => 8, :default => 5
+      t.integer :cunning, :limit => 8, :default => 5
+      t.integer :available_points, :default => 0
+      t.boolean :admin, :default => false
 
       # t.encryptable
       # t.confirmable
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
       # t.token_authenticatable
 
-
       t.timestamps
     end
 
     add_index :users, :email,                :unique => true
+    add_index :users, :username,             :unique => true
     add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
