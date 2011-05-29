1e8ed424264f5db34504eac852e602b1082e1e43
bry <bry@bryanbibat.net>
2011-05-29 11:47:01 +0800

    use 1.8.7 syntax for sort

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,7 +1,6 @@
 class InventoriesController < ApplicationController
   def show
-    @owned_items = current_user.owned_items
-    @owned_items.sort_by! { |item| item.item.item_type }
+    @owned_items = current_user.owned_items.sort_by { |item| item.item.item_type }
   end
 
   def update
